c69750ea9b55b81576e7ea6fa665efe6bcde4c1a
ym4199 <ym4199@gmail.com>
2017-06-13 10:27:49 +0900

    modify post

--- post/urls.py
+++ post/urls.py
@@ -3,13 +3,8 @@ from . import views
 
 
 urlpatterns =[
-<<<<<<< HEAD
     url(r'^$', views.post_list, name='list'),
     url(r'^create/$', views.post_create, name='create'),
     url(r'^(?P<post_pk>\d+)/modify/$', views.post_modify, name='modify'),
     url(r'^(?P<post_pk>\d+)/$', views.post_detail, name ='detail'),
-=======
-    # url(r'^$', views.index),
-    url(r'^$', views.post_list),
->>>>>>> origin/master
 ]
